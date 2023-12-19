0f4c3f541e46e21a96cd678208966d2fab4259b5
Roy Bersaky <roybersaky04@gmail.com>
2023-12-19 03:24:11 +0200

    1

--- api/database/config.js
+++ api/database/config.js
@@ -5,7 +5,7 @@ const config = {
         host: process.env.DB_HOST || "127.0.0.1",
         user: process.env.DB_USER || "root",
         port: 3307,
-        password: process.env.DB_PASS || "Root@2004$",
+        password: process.env.DB_PASS || "root",
         database: process.env.DB_NAME || "csis_228_db",
         connectionLimit: 10,
     }
